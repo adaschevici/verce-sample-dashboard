76696c6358c5f37fcf23fb96c2bd0d563e479903
Artur Das <a.daschevici@gmail.com>
2024-06-24 08:02:45 +0200

    Add form validator

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,24 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string().nonempty(),
+  amount: z.coerce.number().positive(),
+  status: z.enum(['paid', 'pending']),
+  date: z.string().optional(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  }
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  console.log('Creating invoice:', { customerId, amountInCents, status });
 }
 
