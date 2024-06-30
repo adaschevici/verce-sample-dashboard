8f333b2031962ca4ed18f92bdbcace6fec7301dd
Artur Das <a.daschevici@gmail.com>
2024-06-30 11:44:20 +0200

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
 import { inter } from '@/app/ui/fonts';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+
+export const metadata: Metadata = {
+  title: 'My ACME Dashboard',
+  description: 'Descroption of my app',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
