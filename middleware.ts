00e2853bb86ba6df4b26500ba202a422a5c6fad2
Artur Das <a.daschevici@gmail.com>
2024-06-30 18:09:04 +0200

    Add rate limiting middleware, need to configure it

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server';
 import NextAuth from 'next-auth';
+import { Ratelimit } from '@upstash/ratelimit';
+import { kv } from '@vercel/kv';
 import { authConfig } from './auth.config';
 
 export default NextAuth(authConfig).auth;
@@ -6,3 +9,26 @@ export default NextAuth(authConfig).auth;
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  // 5 requests from the same IP in 10 seconds
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+});
+
+// Define which routes you want to rate limit
+export const rateLimitingCfg = {
+  matcher: '/',
+};
+
+export async function rateLimiter(request: NextRequest) {
+  // You could alternatively limit based on user ID or similar
+  const ip = request.ip ?? '127.0.0.1';
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    ip
+  );
+  return success
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL('/blocked', request.url));
+}
